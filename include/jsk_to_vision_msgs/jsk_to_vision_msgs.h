dbe9e4232aa874d2fc21e8ba6e44a9716c9821bf
Masaya Kataoka <ms.kataoka@gmail.com>
2019-09-16 16:37:46 +0900

    add 3d bounding box

--- include/jsk_to_vision_msgs/jsk_to_vision_msgs.h
+++ include/jsk_to_vision_msgs/jsk_to_vision_msgs.h
@@ -5,8 +5,12 @@
 #include <ros/ros.h>
 #include <nodelet/nodelet.h>
 #include <vision_msgs/Detection2DArray.h>
+#include <vision_msgs/Detection3DArray.h>
 #include <jsk_recognition_msgs/RectArray.h>
 #include <jsk_recognition_msgs/BoundingBoxArray.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 
 // Headers in Boost
 #include <boost/optional.hpp>
@@ -16,6 +20,7 @@
 
 namespace jsk_to_vision_msgs
 {
+    typedef message_filters::sync_policies::ApproximateTime<jsk_recognition_msgs::RectArray, jsk_recognition_msgs::BoundingBoxArray> SyncPolicy;
     class JskToVisionMsgs: public nodelet::Nodelet
     {
     protected:
@@ -26,7 +31,11 @@ namespace jsk_to_vision_msgs
         ros::NodeHandle pnh_;
         ros::Subscriber rect_array_sub_;
         std::string rect_array_topic_;
-        ros::Publisher detection_pub_;
+        ros::Publisher detection2d_pub_,detection3d_pub_;
+        std::shared_ptr<message_filters::Subscriber<jsk_recognition_msgs::RectArray> > cluster_rect_sub_;
+        std::shared_ptr<message_filters::Subscriber<jsk_recognition_msgs::BoundingBoxArray> > cluster_bbox_sub_;
+        std::shared_ptr<message_filters::Synchronizer<SyncPolicy> > sync_ptr_;
+        void callback(const jsk_recognition_msgs::RectArray::ConstPtr jsk_rect,const jsk_recognition_msgs::BoundingBoxArray::ConstPtr jsk_bbox);
     };
 }
 
--- src/jsk_to_vision_msgs.cpp
+++ src/jsk_to_vision_msgs.cpp
@@ -6,27 +6,41 @@ namespace jsk_to_vision_msgs
     {
         nh_ = getNodeHandle();
         pnh_ = getPrivateNodeHandle();
-        detection_pub_ = pnh_.advertise<vision_msgs::Detection2DArray>("output",1);
-        rect_array_sub_ = pnh_.subscribe("input",1,&JskToVisionMsgs::rectArrayCallback,this);
+        detection2d_pub_ = pnh_.advertise<vision_msgs::Detection2DArray>("output/detection_2d",1);
+        detection3d_pub_ = pnh_.advertise<vision_msgs::Detection3DArray>("output/detection_3d",1);
+        cluster_rect_sub_ = std::make_shared<message_filters::Subscriber<jsk_recognition_msgs::RectArray> >(pnh_, "input/rect", 10);
+        cluster_bbox_sub_ = std::make_shared<message_filters::Subscriber<jsk_recognition_msgs::BoundingBoxArray> >(pnh_, "input/bbox", 10);
+        sync_ptr_ = std::make_shared<message_filters::Synchronizer<SyncPolicy> >(SyncPolicy(10), *cluster_rect_sub_ , *cluster_bbox_sub_);
+        sync_ptr_->registerCallback(boost::bind(&JskToVisionMsgs::callback, this, _1, _2));
     }
 
-    void JskToVisionMsgs::rectArrayCallback(const jsk_recognition_msgs::RectArray::ConstPtr msg)
+    void JskToVisionMsgs::callback(const jsk_recognition_msgs::RectArray::ConstPtr jsk_rect,const jsk_recognition_msgs::BoundingBoxArray::ConstPtr jsk_bbox)
     {
-        vision_msgs::Detection2DArray detection_msg;
-        detection_msg.header = msg->header;
-        for(auto itr = msg->rects.begin(); itr != msg->rects.end(); itr++)
+        vision_msgs::Detection2DArray detection2d_msg;
+        detection2d_msg.header = jsk_rect->header;
+        for(auto itr = jsk_rect->rects.begin(); itr != jsk_rect->rects.end(); itr++)
         {
             vision_msgs::Detection2D detection;
-            detection.header = msg->header;
+            detection.header = jsk_rect->header;
             detection.bbox.center.theta = 0;
             detection.bbox.center.x = (double)itr->x+0.5*(double)itr->width;
             detection.bbox.center.y = (double)itr->y+0.5*(double)itr->height;
             detection.bbox.size_x = (double)itr->width;
             detection.bbox.size_y = (double)itr->height;
             detection.is_tracking = false;
-            detection_msg.detections.push_back(detection);
+            detection2d_msg.detections.push_back(detection);
         }
-        detection_pub_.publish(detection_msg);
+        vision_msgs::Detection3DArray detection3d_msg;
+        detection3d_msg.header = jsk_bbox->header;
+        for(auto itr = jsk_bbox->boxes.begin(); itr != jsk_bbox->boxes.end(); itr++)
+        {
+            vision_msgs::Detection3D detection;
+            detection.bbox.center = itr->pose;
+            detection.bbox.size = itr->dimensions;
+            detection3d_msg.detections.push_back(detection);
+        }
+        detection2d_pub_.publish(detection2d_msg);
+        detection3d_pub_.publish(detection3d_msg);
     }
 }
 
